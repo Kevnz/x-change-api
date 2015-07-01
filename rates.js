92cc185f9cf8faac9be4b59fe3ab1b3ff7e78154
Kevin Isom <kevin.isom@gmail.com>
2015-07-01 19:44:43 +1200

    use redis to cache the results, querying the service is expense

--- rates.js
+++ rates.js
@@ -1,3 +1,4 @@
+'use strict';
 var oxr = require('open-exchange-rates'),
     fx = require('money');
 var config = require('xtconf')();
@@ -5,6 +6,8 @@ var async = require('async');
 var daylight = require('daylight');
 var dateMath = require('date-arithmetic');
 var utils = require('./utils');
+var redis = require('redis');
+
 
 var base_amount = 1;
 oxr.set({ app_id: config.get('app_id')});
@@ -29,12 +32,24 @@ function getRate (date, callback) {
 
 
 function getRateFuncForCurrencies(from, to) {
+    var client = redis.createClient(config.get('redis-port'), config.get('redis-endpoint'));
+    client.auth(config.get('redis-password'));
     return function (date, callback) {
-    	oxr.historical(date, function() {
-    	    fx.rates = oxr.rates;
-    	    fx.base = oxr.base; 
-    		callback(null, {date: date, result: fx(base_amount).from(from).to(to)});
-    	});
+
+        client.get('rate-from-' + from + '-to-' + to + '-date-' + date, function (err, reply) {
+            if(reply !== null) {
+                callback(null, { date: date, result: reply });
+            } else {
+                oxr.historical(date, function() {
+                    fx.rates = oxr.rates;
+                    fx.base = oxr.base; 
+                    var result = fx(base_amount).from(from).to(to); 
+                    client.set('rate-from-' + from + '-to-' + to + '-date-' + date, result, function (err, reply) {
+                        callback(null, {date: date, result: result});
+                    });
+                });
+            }
+        });
     }
 }
 
@@ -43,7 +58,7 @@ module.exports = {
 
         var dateRange =  utils.yearPoints(Number(year));
         console.log(dateRange);
-        var rateFunc = getRateFuncForCurrencies(from,to);
+        var rateFunc = getRateFuncForCurrencies(from, to);
         var ymd = utils.yearMonthDay(Date.now());
         if (year === ymd.year) {
             var _oldCallback = callback;
